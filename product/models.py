cfa499b4948592427a4985a4b112fd4208b2beb3
ErlJocson <jocsonerl@gmail.com>
2022-04-13 17:15:21 +0800

    added forms and designs

--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from itertools import product
 from django.db import models
 from django.contrib.auth.models import User
@@ -7,7 +8,7 @@ class Product(models.Model):
     details = models.TextField()
     price   = models.DecimalField(max_digits=1000, decimal_places=2)
     date    = models.DateTimeField(auto_now_add=True)
-    stock   = models.IntegerField()
+    stock   = models.IntegerField(default=1)
     image   = models.ImageField()
     seller  = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- product/views.py
+++ product/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
+from .models import Order, Product
 
-# Create your views here.
 def home_view(request):
+    products = Product.objects.exclude(id=request.user.id)
     return render(request, "home_view.html", {
-        "title":"Home"
+        "title":"Home",
+        "products":products,
     })
 
 def store_view(request):
+    store = Product.objects.filter(id=request.user.id)
     return render(request, "store_view.html", {
-        "title":"Store"
+        "title":"Store",
+        "store":store,
     })
 
 def order_view(request):
+    orders = Order.objects.filter(id=request.user.id)
     return render(request, "order_view.html", {
-        "title":"Order"
+        "title":"Order",
+        "orders":orders,
     })
