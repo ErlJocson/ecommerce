8c765dfceed3c8531ce23bc2761307f88abec5f5
ErlJocson <jocsonerl@gmail.com>
2022-04-22 18:45:05 +0800

    changes in image path name

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('', include("product.urls")),
     path('auth/', include("user.urls")),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
